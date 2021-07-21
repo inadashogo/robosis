7182a65a9f21a285539289993901ab62a33b021c
Inada shogo <s20C1014VR@s.chibakoudai.jp>
2021-07-21 19:48:03 +0000

    Change program

--- myled.c
+++ myled.c
@@ -1,3 +1,8 @@
+//SPDX-License-Identifier:GPL-3.0
+////*
+// * Copyright(c)2021 Ryuich Ueda. Yuma Ito. All rights reserved. 
+// * /
+
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
